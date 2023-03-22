c8263a783c6520def95b744569f24a37255f15fd
Pierre Genthon <genthon.pierre@gmail.com>
2023-03-22 10:51:17 +0100

    test wilder component

--- client/src/components/Wilder.spec.tsx
+++ client/src/components/Wilder.spec.tsx
@@ -11,17 +11,13 @@ const adminProfileMock = {
   },
   result: {
     data: {
-      profile: {
-        id: "1",
-        email: "admin@gmail.com",
-        role: "admin",
-      },
+      profile: { id: "1", email: "admin@website.com", role: "admin" },
     },
   },
 };
 
 describe("Wilder component", () => {
-  it("renders name and skills", () => {
+  it("should properly render the wilder passed in prop", () => {
     const view = render(
       <MockedProvider mocks={[adminProfileMock]} addTypename={false}>
         <Wilder
@@ -30,102 +26,24 @@ describe("Wilder component", () => {
             name: "Dave",
             skills: [
               { id: 1, name: "PHP", votes: 3 },
-              { id: 2, name: "JS", votes: 5 },
+              { id: 2, name: "JS", votes: 7 },
             ],
           }}
-        ></Wilder>
+        />
       </MockedProvider>,
       { wrapper: BrowserRouter }
     );
+
     expect(screen.getByText(/Dave/)).toBeVisible();
     expect(screen.getByText(/PHP/)).toBeVisible();
     expect(screen.getByText(/JS/)).toBeVisible();
     expect(screen.getByText(/3/)).toBeVisible();
-    expect(screen.getByText(/5/)).toBeVisible();
-    expect(view.baseElement).toMatchInlineSnapshot(`
-<body>
-  <div>
-    <div
-      class="flex bg-white p-4 rounded-2xl mb-4 shadow-md"
-    >
-      <a
-        href="/wilders/1"
-      >
-        <img
-          alt="Dave"
-          class="h-16 w-16 rounded-full mr-6"
-          src="avatar.png"
-        />
-      </a>
-      <div
-        class="flex justify-between w-full  min-w-[200px]"
-      >
-        <div
-          class="flex flex-col"
-        >
-          <a
-            href="/wilders/1"
-          >
-            <h3
-              class="font-semibold"
-            >
-              Dave
-            </h3>
-          </a>
-          <ul
-            class="flex flex-wrap"
-          >
-            <li
-              class="transition-all duration-500 flex items-center mr-4 mb-3 bg-black/5 hover:bg-black/10 p-1 pl-2 pr-2 rounded-xl cursor-pointer border hover:border-black/5 "
-            >
-              JS
-              <div
-                class="items-center flex bg-black/30 text-white rounded-[9999px] h-[25px] pr-2 pl-2 justify-center ml-2 transition-all duration-1000"
-              >
-                <div
-                  class="transition-all duration-1000"
-                >
-                  5
-                </div>
-              </div>
-            </li>
-            <li
-              class="transition-all duration-500 flex items-center mr-4 mb-3 bg-black/5 hover:bg-black/10 p-1 pl-2 pr-2 rounded-xl cursor-pointer border hover:border-black/5 "
-            >
-              PHP
-              <div
-                class="items-center flex bg-black/30 text-white rounded-[9999px] h-[25px] pr-2 pl-2 justify-center ml-2 transition-all duration-1000"
-              >
-                <div
-                  class="transition-all duration-1000"
-                >
-                  3
-                </div>
-              </div>
-            </li>
-          </ul>
-        </div>
-        <div
-          class="flex flex-col min-w-[40px]"
-        >
-          <a
-            href="/wilders/1/edit"
-          >
-            <button
-              class="mb-2 w-full"
-            >
-              ✏️
-            </button>
-          </a>
-        </div>
-      </div>
-    </div>
-  </div>
-</body>
-`);
+    expect(screen.getByText(/7/)).toBeVisible();
+
+    expect(view.baseElement).toMatchSnapshot();
   });
 
-  it("renders a button to delete the wilder when logged in as admin", async () => {
+  it("should display a delete button when logged in with admin", async () => {
     render(
       <MockedProvider mocks={[adminProfileMock]} addTypename={false}>
         <Wilder
@@ -134,10 +52,11 @@ describe("Wilder component", () => {
             name: "Dave",
             skills: [],
           }}
-        ></Wilder>
+        />
       </MockedProvider>,
       { wrapper: BrowserRouter }
     );
+
     expect(await screen.findByText(/x/)).toBeVisible();
   });
 });
