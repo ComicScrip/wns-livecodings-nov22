e250cc05be17ff1fd5de1059f9a4295ec808c9b1
Pierre Genthon <genthon.pierre@gmail.com>
2023-03-22 13:26:39 +0100

    remove lc contents

--- integration-tests/src/wilderResolver.test.ts
+++ integration-tests/src/wilderResolver.test.ts
@@ -1,8 +1,9 @@
-import { gql } from "@apollo/client/core";
-import Wilder from "../../server/src/entity/Wilder";
-import client from "./apolloClient";
-import db from "../../server/src/db";
+// import { gql } from "@apollo/client/core";
+// import Wilder from "../../server/src/entity/Wilder";
+// import client from "./apolloClient";
+// import db from "../../server/src/db";
 
+/*
 const createWilderMutation = gql`
   mutation CreateWilder($data: WilderInput!) {
     createWilder(data: $data) {
@@ -20,45 +21,16 @@ const readWildersQuery = gql`
     }
   }
 `;
+*/
 
 describe("Wilder resolver", () => {
   describe("create wilder", () => {
-    it("should create wilder given valid attributes", async () => {
-      const res = await client.mutate({
-        mutation: createWilderMutation,
-        variables: { data: { name: "Dave" } },
-      });
+    xit("should create a wilder given valid attributes", async () => {});
 
-      expect(res.data?.createWilder).toHaveProperty("id");
-      expect(res.data?.createWilder).toHaveProperty("name", "Dave");
-    });
-
-    it("should not create wilder given invalid attributes and return an error", async () => {
-      expect(() =>
-        client.mutate({
-          mutation: createWilderMutation,
-          variables: { data: { name: "" } },
-        })
-      ).rejects.toThrowErrorMatchingInlineSnapshot(
-        `"Argument Validation Error"`
-      );
-    });
+    xit("should not create wilder given invalid attributes and return an error", async () => {});
   });
 
   describe("read wilders", () => {
-    it("should return an array", async () => {
-      await db
-        .getRepository(Wilder)
-        .insert([{ name: "jojo" }, { name: "jaja" }]);
-
-      const res = await client.query({
-        query: readWildersQuery,
-        fetchPolicy: "no-cache",
-      });
-
-      expect(res.data.wilders.length).toBe(2);
-      expect(res.data.wilders[0]).toHaveProperty("id");
-      expect(res.data.wilders[0]).toHaveProperty("name");
-    });
+    xit("should return an array", async () => {});
   });
 });
